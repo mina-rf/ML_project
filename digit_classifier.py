997f5f304dcb78a4997d2f4ae1baebaa55cd3b42
mina-rf <rafiei.mina73@gmail.com>
2018-01-28 19:32:42 +0330

    hog + pca + svm

--- digit_classifier.py
+++ digit_classifier.py
@@ -1,11 +1,16 @@
 from mnist import MNIST
 import numpy as np
 from skimage.feature import hog
+from sklearn.decomposition import PCA
+from sklearn import svm
+
 
 mndata = MNIST('data')
 tr_imgs , tr_lbs = mndata.load_training()
 test_imgs , test_lbs = mndata.load_testing()
 
+new_tr_lbs = [item for item in tr_lbs]
+new_test_label = [item for item in test_lbs]
 
 
 new_train = []
@@ -15,5 +20,18 @@ for img in tr_imgs :
     img_hog = hog(pixels)
     new_train.append(img_hog)
 
+new_test = []
+for img in test_imgs :
+    pixels = np.array(img, dtype='uint8')
+    pixels = pixels.reshape((28, 28))
+    img_hog = hog(pixels)
+    new_test.append(img_hog)
+
+print(new_train[0])
+pca = PCA(n_components=5)
+pca.fit(new_train).transform(new_train)
 
 
+clf = svm.SVC(decision_function_shape='ova')
+clf.fit(tr_imgs, new_tr_lbs)
+print(clf.score(test_imgs,new_test_label))
